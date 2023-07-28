aad7adb3d8f8ab8e6ad7ad4e50e023e516f957fe
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-28 14:03:16 +0530

    / mapped

--- src/main/java/com/springmvc/luv/controllers/HomeController.java
+++ src/main/java/com/springmvc/luv/controllers/HomeController.java
@@ -1,5 +1,16 @@
 package com.springmvc.luv.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	@ResponseBody
+	public String homeController()
+	{
+		return "We are Home";
+	}
 }
