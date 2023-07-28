b2a4ca3e359d7f013bc1a20afdf01ba0abc41169
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-28 16:00:14 +0530

    /process-Home handled & added fetched data to view

--- src/main/java/com/springmvc/luv/controllers/HomeController.java
+++ src/main/java/com/springmvc/luv/controllers/HomeController.java
@@ -1,16 +1,28 @@
 package com.springmvc.luv.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	@ResponseBody
 	public String homeController()
 	{
-		return "We are Home";
+		return "homePage";
+	}
+	
+	@RequestMapping("/process-Home")
+	public String processHome(@RequestParam String txtuname , 
+			@RequestParam("txtcrush") String crush,
+			Model model)
+	{
+		model.addAttribute("name", txtuname);
+		model.addAttribute("crush", crush);
+		System.out.println("Your Name: "+ txtuname );
+		System.out.println("Crush Name: "+ crush );
+		return "homeResultPage";
 	}
 }
