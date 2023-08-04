a66062683121316712f0e5435291f64a0b63191b
ritesh6916 <riteshsingh0500@gmail.com>
2023-08-05 01:29:54 +0530

    fields added

--- src/main/java/com/springmvc/luv/model/HomeModel.java
+++ src/main/java/com/springmvc/luv/model/HomeModel.java
@@ -1,5 +1,33 @@
 package com.springmvc.luv.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
 public class HomeModel {
 
+	private String txtuname="Mr X";
+	private String txtcrush="Miss Y";
+	
+	public HomeModel() {
+		System.out.println("HomeModel being called..");		
+	}
+	public String getTxtuname() {
+		return txtuname;
+	}
+	public void setTxtuname(String txtuname) {
+		this.txtuname = txtuname;
+	}
+	public String getTxtcrush() {
+		return txtcrush;
+	}
+	public void setTxtcrush(String txtcrush) {
+		this.txtcrush = txtcrush;
+	}
+	@Override
+	public String toString() {
+		return "HomeModel [txtuname=" + txtuname + ", txtcrush=" + txtcrush + "]";
+	}
+	
 }
