dbaf543142f6ac0b14b9b8eed7a87af7e7c7f787
ritesh6916 <riteshsingh0500@gmail.com>
2023-08-05 23:32:08 +0530

    url mapped

--- src/main/java/com/springmvc/luv/controllers/RegistrationController.java
+++ src/main/java/com/springmvc/luv/controllers/RegistrationController.java
@@ -1,5 +1,15 @@
 package com.springmvc.luv.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RegistrationController {
 
+	@RequestMapping("/register")
+	public String showRegistrationPage()
+	{
+		
+		return "registrationPageHome";
+	}
 }
