0ced293f56814a85bfbd0609bed4b588088a31be
ritesh6916 <riteshsingh0500@gmail.com>
2023-08-05 23:51:56 +0530

    Model added

--- src/main/java/com/springmvc/luv/controllers/RegistrationController.java
+++ src/main/java/com/springmvc/luv/controllers/RegistrationController.java
@@ -1,15 +1,20 @@
 package com.springmvc.luv.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springmvc.luv.model.RegistrationModel;
 
 @Controller
 public class RegistrationController {
 
+	
 	@RequestMapping("/register")
-	public String showRegistrationPage()
+	public String showRegistrationPage( Model model)
 	{
-		
+		RegistrationModel registrationModel=new RegistrationModel();
+		model.addAttribute("registrationModel", registrationModel);
 		return "registrationPageHome";
 	}
 }
