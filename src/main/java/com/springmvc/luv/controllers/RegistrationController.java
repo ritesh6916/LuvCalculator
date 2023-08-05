1bce7fe887233f602aebc056b4ebcd766d79e70a
ritesh6916 <riteshsingh0500@gmail.com>
2023-08-05 23:56:09 +0530

    moved to @ModelAttribute

--- src/main/java/com/springmvc/luv/controllers/RegistrationController.java
+++ src/main/java/com/springmvc/luv/controllers/RegistrationController.java
@@ -2,6 +2,7 @@ package com.springmvc.luv.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springmvc.luv.model.RegistrationModel;
@@ -11,10 +12,10 @@ public class RegistrationController {
 
 	
 	@RequestMapping("/register")
-	public String showRegistrationPage( Model model)
-	{
-		RegistrationModel registrationModel=new RegistrationModel();
-		model.addAttribute("registrationModel", registrationModel);
+	public String showRegistrationPage(@ModelAttribute("registrationModel") RegistrationModel registrationModel) {
+		
+		registrationModel.setTxtname("Ritesh");
+		
 		return "registrationPageHome";
 	}
 }
