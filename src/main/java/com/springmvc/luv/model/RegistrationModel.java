a94cee28ccf5aba8cd6f4c9f25411ee2bb78d09c
ritesh6916 <riteshsingh0500@gmail.com>
2023-08-05 23:31:30 +0530

    initial

---
+++ src/main/java/com/springmvc/luv/model/RegistrationModel.java
@@ -0,0 +1,26 @@
+package com.springmvc.luv.model;
+
+public class RegistrationModel {
+	private String txtname;
+	private String txtuser;
+	private char[] txtpass;
+	public String getTxtname() {
+		return txtname;
+	}
+	public void setTxtname(String txtname) {
+		this.txtname = txtname;
+	}
+	public String getTxtuser() {
+		return txtuser;
+	}
+	public void setTxtuser(String txtuser) {
+		this.txtuser = txtuser;
+	}
+	public char[] getTxtpass() {
+		return txtpass;
+	}
+	public void setTxtpass(char[] txtpass) {
+		this.txtpass = txtpass;
+	}
+	
+}
